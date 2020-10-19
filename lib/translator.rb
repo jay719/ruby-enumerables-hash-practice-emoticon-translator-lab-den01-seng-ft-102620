0a2024a21bacc313e9bec0a068dc8868ea6f97f1
Javaria Brascom <javariab17@gmail.com>
2020-10-19 03:16:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 def get_japanese_emoticon(path,emoticon)
   load_library(path).each do |key, value|
     if value[:english] == emoticon
-      return value[:japenese]
+      return value[:japanese]
   
 end
 
